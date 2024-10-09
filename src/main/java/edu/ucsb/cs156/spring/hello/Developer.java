4c837720ae99cb6c2b8e3dfd8bd8942f280da677
Yixuan Wong <yixuanwong@ucsb.edu>
2024-10-08 18:14:44 -0700

    yw - updated with actual team member names

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -34,14 +34,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("f24-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f24-12");
+        team.addMember("Abby");
+        team.addMember("Javin");
+        team.addMember("Maya");
+        team.addMember("Sarvesh");
+        team.addMember("Yixuan");
+        team.addMember("Yungong");
         return team;
     }
 }
