4047b570a37322c44e61d1550c0c77871a255bf0
Yixuan Wong <yixuanwong@ucsb.edu>
2024-10-08 18:09:15 -0700

    yw - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,10 @@ public class DeveloperTest {
         assertEquals("Yixuan", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("yixuan-wong", Developer.getGithubId());
+    }
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
